a9e59330dfd57e748fd77f15f69c6721ef8df630
Nelson Samuel <106776244+samuelnelson176@users.noreply.github.com>
2024-04-03 20:34:52 -0700

    Add files via upload

---
+++ SNFT/index.js
@@ -0,0 +1,14 @@
+
+function toggle() {
+    let hamMenu = document.querySelector('.ham-menu');
+    hamMenu.classList.toggle('hidden'); 
+  }
+  cross = document.querySelector('.hamMenu');
+
+  if (document.cross.display.style.display = "none") {
+    document.cross.display.style.display = "block"
+  }
+
+  else  {
+    document.cross.display.style.display = "block"
+  }
